bf4e7f883981a8aee6418997780a03c413c47f93
Attila Hegyessy <attila.hegyessy@gmail.com>
2020-10-11 12:46:21 +0200

    Updates

---
+++ precache-manifest.9bb26b68a0340de272c59212702467a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e02e4a57a0020707d1fdaaaa8bdd393",
+    "url": "/MarvelChampionsLCGGenerator/index.html"
+  },
+  {
+    "revision": "5cb0dd69618b2fb714fd",
+    "url": "/MarvelChampionsLCGGenerator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "32724ba4ae28c01d62bb",
+    "url": "/MarvelChampionsLCGGenerator/static/js/2.f3d799e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MarvelChampionsLCGGenerator/static/js/2.f3d799e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb0dd69618b2fb714fd",
+    "url": "/MarvelChampionsLCGGenerator/static/js/main.95df38f1.chunk.js"
+  },
+  {
+    "revision": "a982fe6ac28901d16874",
+    "url": "/MarvelChampionsLCGGenerator/static/js/runtime-main.abcee005.js"
+  }
+]);
